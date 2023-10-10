39e306b8002230d74af504dd4175cf58b6fc24b3
Adeolu Amole <adeoluamole@gmail.com>
2023-10-10 08:23:51 +0100

    Working on the project perfection

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 3001;
 
 
 
