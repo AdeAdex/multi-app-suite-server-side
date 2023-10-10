8f56c109e6e4d269f1be7baeab0147df030471bc
Adeolu Amole <adeoluamole@gmail.com>
2023-10-10 08:54:35 +0100

    Working on the project perfection

--- index.js
+++ index.js
@@ -1,75 +1,34 @@
 //backend
 
-require('dotenv').config();
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-const app = express();
-const port = process.env.PORT || 3001;
-
-
-app.use(express.json());
-
-const corsOptions = {
-  origin: ["http://localhost:4000", "https://adex-multi-app-suite.vercel.app"],
-};
-
-app.use(cors(corsOptions));
-
-app.get("/api/football-matches", async (req, res) => {
-  console.log("adex");
-  try {
-    const response = await axios.get(
-      "https://api.football-data.org/v4/matches",
-      {
-        headers: {
-          "X-Auth-Token": "70f5fc17b1374351b458e3f71cb76249",
-        },
-      }
-    );
-
-  console.log("adex adex");
-    console.log(response);
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-
-
+// require('dotenv').config();
 // const express = require("express");
 // const axios = require("axios");
 // const cors = require("cors");
 // const app = express();
 // const port = process.env.PORT || 3001;
 
+
 // app.use(express.json());
 
-// // Enable CORS for specific origin
 // const corsOptions = {
-//   origin: ["http://localhost:3000"],
+//   origin: ["http://localhost:4000", "https://adex-multi-app-suite.vercel.app"],
 // };
 
 // app.use(cors(corsOptions));
 
-// // Your endpoint to fetch football matches
 // app.get("/api/football-matches", async (req, res) => {
+//   console.log("adex");
 //   try {
-//     // Make the GET request to the API-Football endpoint
-//     const response = await axios.get("https://api-football-v1.p.rapidapi.com/v3/timezone", {
-//       headers: {
-//         'X-RapidAPI-Key': 'ed866feae9msh3d8b64547ec32aep107d87jsn37af4db87ca7',
-//         'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+//     const response = await axios.get(
+//       "https://api.football-data.org/v4/matches",
+//       {
+//         headers: {
+//           "X-Auth-Token": "70f5fc17b1374351b458e3f71cb76249",
+//         },
 //       }
-//     });
+//     );
 
-//     // Respond with the data from the API-Football endpoint
+//   console.log("adex adex");
 //     console.log(response);
 //     res.json(response.data);
 //   } catch (error) {
@@ -81,3 +40,41 @@ app.listen(port, () => {
 // app.listen(port, () => {
 //   console.log(`Server is running on port ${port}`);
 // });
+
+
+require('dotenv').config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+const corsOptions = {
+  origin: ["http://localhost:4000", "https://adex-multi-app-suite.vercel.app"],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/api/football-matches", async (req, res) => {
+  try {
+    const response = await axios.get("https://api-football-v1.p.rapidapi.com/v3/timezone", {
+      headers: {
+        'X-RapidAPI-Key': 'ed866feae9msh3d8b64547ec32aep107d87jsn37af4db87ca7',
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+      }
+    });
+
+    // Respond with the data from the API-Football endpoint
+    console.log(response);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
